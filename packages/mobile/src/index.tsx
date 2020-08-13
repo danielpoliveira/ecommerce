0d400f1d876d8ce61705ff8974a4084be0233ea6
danielpoliveira <dani.edm@outlook.com>
2020-08-12 23:48:44 -0300

    others changes

--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import Label from '@components/TextFields/Label';
+import Dropdown from '@components/Dropdown';
+import Checkbox from '@components/Checkbox';
+import Switch from '@components/Switches';
+import TagOrdinary from '@components/Tags/Ordinary';
+import TagSmall from '@components/Tags/Small';
+import Hotlabel from '@components/Hotlabel';
+
+
 
 const App: React.FC = () => {
   return (
@@ -10,9 +18,8 @@ const App: React.FC = () => {
       backgroundColor: '#1E1F28',
       alignItems: "center"
     }}>
-
-
-      <Label />
+      
+      <Hotlabel active />
     </View>
   );
 }
