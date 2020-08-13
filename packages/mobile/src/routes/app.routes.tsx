ff57c67e0044a88018ea0c5653d6dca4f42d734a
danielpoliveira <dani.edm@outlook.com>
2020-08-13 03:57:24 -0300

    Added bottom-tab navigation to appstack routes

--- packages/mobile/src/routes/app.routes.tsx
+++ packages/mobile/src/routes/app.routes.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const AppStack = createStackNavigator();
+import Home from '@pages/Home';
 
+const AppStack = createBottomTabNavigator();
 
 const AppRoutes: React.FC = () => {
   return (
-    <AppStack.Navigator>
-      {/**<AppStack.Screen /> */}
+    <AppStack.Navigator
+
+
+      tabBarOptions={{
+        style: {
+          backgroundColor: '#1E1F28',
+          borderTopWidth: 0,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+          
+        },
+
+      }} >
+
+      <AppStack.Screen name="Home" component={Home} />
     </AppStack.Navigator>
   );
 }
