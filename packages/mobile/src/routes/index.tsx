787df5624d32eb2643d3bbed18430bd4f49fd690
danielpoliveira <dani.edm@outlook.com>
2020-08-13 02:17:54 -0300

    Added routes base

---
+++ packages/mobile/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { useAuth  } from '@contexts/auth';
+
+import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
+
+
+const Routes: React.FC = () => {
+  const { signed } = useAuth();
+
+   return signed? <AppRoutes /> : <AuthRoutes />
+
+
+
+}
+
+
+export default Routes;
