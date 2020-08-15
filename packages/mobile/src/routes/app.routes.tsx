65f41ce4d907149426b5481c7d79b0101a55a3ee
danielpoliveira <dani.edm@outlook.com>
2020-08-14 23:36:31 -0300

    change styles of bottom tab bar

--- packages/mobile/src/routes/app.routes.tsx
+++ packages/mobile/src/routes/app.routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import HomeIcon from '@components/BottomTab/Icons/HomeIcon';
+
 import Home from '@pages/Home';
 
 const AppStack = createBottomTabNavigator();
@@ -9,20 +11,35 @@ const AppStack = createBottomTabNavigator();
 const AppRoutes: React.FC = () => {
   return (
     <AppStack.Navigator
-
+      
 
       tabBarOptions={{
+        activeTintColor: '#EF3651',
+        inactiveTintColor: '#fff',
+        labelStyle: {
+          fontSize: 10,
+          lineHeight: 10,
+          fontFamily: 'Poppins-SemiBold',
+        },
+
         style: {
           backgroundColor: '#1E1F28',
           borderTopWidth: 0,
           borderTopLeftRadius: 12,
           borderTopRightRadius: 12,
-          
         },
 
       }} >
 
-      <AppStack.Screen name="Home" component={Home} />
+      <AppStack.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+          
+
+        }} 
+      />
+
+   
+
     </AppStack.Navigator>
   );
 }
