ab2d58e3da15e29b9f7602b18740c8d3fff5ba61
danielpoliveira <dani.edm@outlook.com>
2020-08-14 23:37:27 -0300

    created HomeIcon and Profile icon component of bottom tab bar

---
+++ packages/mobile/src/components/BottomTab/Icons/ProfileIcon/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Icon from '@svg/profile/Profile.svg'
+
+const ProfileIcon: React.FC = props => {
+  
+  const { color } = props;
+
+  return (
+    <View style={styles.container} >
+      <Icon fill={color}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 30, height: 30,
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
+
+export default ProfileIcon;
