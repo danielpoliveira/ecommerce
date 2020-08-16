92883981090b91e4efe000e7c0e2fac2d3c3de94
danielpoliveira <dani.edm@outlook.com>
2020-08-16 03:00:50 -0300

    created 'shoping bag' page

--- packages/mobile/src/pages/Bag/index.tsx
+++ packages/mobile/src/pages/Bag/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, StatusBar, ScrollView,
+} from 'react-native';
 import ProductCartBag from '@components/ProductCard/Bag';
+import Title from '@components/Title';
 
 const Bag: React.FC = (props) => (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }} >
-      <ProductCartBag/>
-    </View>
+
+  <View style={{
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    paddingHorizontal: 15,
+  }}>
+    <Title placeholder="My Bag" />
+
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <ProductCartBag />
+      <ProductCartBag />
+      <ProductCartBag />
+      <ProductCartBag />
+      <ProductCartBag end />
+    </ScrollView>
+  </View>
 );
 
 export default Bag;
