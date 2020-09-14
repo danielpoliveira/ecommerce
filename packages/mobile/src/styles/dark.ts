13d79e1ce4e07a24590c2299f628a7f4c9d5dafb
danielpoliveira <dani.edm@outlook.com>
2020-09-14 17:02:40 -0300

    Add styles properties to light and dark themes

---
+++ packages/mobile/src/styles/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  mode: 'dark',
+  background: '#2A2C36',
+  title: '#F6F6F6',
+  textColor: '#f6f6f6',
+  textColorSecundary: '#ABB4BD',
+  bottomBarBackground: '#1E1F28',
+  barStyle: 'light-content',
+  error: '#FF2424',
+  primary: '#EF3651',
+  success: '#55D85A',
+  lineColor: '#ABB4BD0D'
+};
+//background: '#1E1F28',
---
+++ packages/mobile/src/styles/light.ts
@@ -0,0 +1,20 @@
+export default {
+  mode: 'light',
+  background: '#F9F9F9',
+  title: '#222222',
+  textColor: '#222222',
+  textColorSecundary: '#9B9B9B',
+  bottomBarBackground: '#FFFFFF',
+  barStyle: 'dark-content',
+  error: '#FF2424',
+  primary: '#DB3022',
+  success: '#55D85A',
+  lineColor: '#9B9B9B5C'
+};
+
+
+
+
+
+
+
