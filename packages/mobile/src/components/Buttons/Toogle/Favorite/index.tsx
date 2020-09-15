7cd667552d10a103c4d08f9c93a0c96e18a2654e
danielpoliveira <dani.edm@outlook.com>
2020-09-14 23:56:27 -0300

    Added function to press button and change favorite style

--- packages/mobile/src/components/Buttons/Toogle/Favorite/index.tsx
+++ packages/mobile/src/components/Buttons/Toogle/Favorite/index.tsx
@@ -1,34 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
 import IonicIcons from 'react-native-vector-icons/Ionicons';
 
 import { useTheme } from '@contexts/theme';
 
-const FavoriteIcon: React.FC = props => {
+const FavoriteIcon = props => {
   const { isEnabled } = props;
   const { theme } = useTheme();
 
+  const [pressed, setPressed] = useState(isEnabled);
+
+  const handleFavorite = () => {
+    setTimeout(() => setPressed(previousState => !previousState), 500);
+  }
+
   return (
-    <TouchableOpacity style={[styles.container, {backgroundColor: theme.background}]}>
-      <IonicIcons   
-        name={`heart-${isEnabled? 'sharp': 'outline'}`} 
-        color={isEnabled? '#EF3651':theme.textColorSecundary} 
-        size={15}
-      />
-    </TouchableOpacity>
+    <View style={styles.rippleMaxArea}>
+      <Pressable 
+        android_ripple={{ color: theme.primary, borderless: false }} 
+        style={[styles.container, { backgroundColor: theme.background }]}
+        onPress={handleFavorite}
+      >
+        <IonicIcons
+          name={`heart-${pressed ? 'sharp' : 'outline'}`}
+          color={pressed ? '#EF3651' : theme.textColorSecundary}
+          size={15}
+        />
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  rippleMaxArea: {
+    overflow: "hidden",
+    borderRadius: 36
+  },
+
   container: {
     flexDirection: 'row',
     width: 36,
     height: 36,
-    
+
     borderRadius: 36,
     alignItems: 'center',
     justifyContent: 'center',
-    
+
     shadowOpacity: 0.35,
     shadowOffset: {
       width: 0,
