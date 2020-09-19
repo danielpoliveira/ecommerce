4d0e9a4bb27a9d513c0c6d0211129019fe6bea2c
danielpoliveira <dani.edm@outlook.com>
2020-09-18 23:11:25 -0300

    Created 'BottomTabNavigation'routes

---
+++ packages/mobile/src/routes/bottomNavigation.routes.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from '@pages/Home';
+import Shop from '@pages/Shop';
+import Bag from '@pages/Bag';
+import Profile from '@pages/Profile';
+
+import HomeIcon from '@components/BottomTab/Icons/HomeIcon';
+import ShopIcon from '@components/BottomTab/Icons/ShopIcon';
+import BagIcon from '@components/BottomTab/Icons/BagIcon';
+import ProfileIcon from '@components/BottomTab/Icons/ProfileIcon';
+
+import { useTheme } from '@contexts/theme';
+
+const BottomStack = createBottomTabNavigator();
+
+const Tabs = () => {
+  const { theme } = useTheme();
+
+  return (
+    <BottomStack.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: theme.primary,
+        inactiveTintColor: theme.textColorSecundary,
+
+        iconStyle: {
+          //marginTop: 2,
+        },
+
+        labelStyle: {
+          //fontSize: 12,
+          //margin: 0,
+          //padding: 0,
+          fontFamily: 'Poppins-SemiBold',
+        },
+
+        style: {
+          height: 60,
+          backgroundColor: theme.bottomBarBackground,
+          borderTopWidth: 0,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        },
+      }} >
+
+      <BottomStack.Screen name="Home" component={Home}
+        options={{
+          tabBarIcon: ({ color, focused }) => <HomeIcon bgColor={focused ? color : 'none'} color={color} />,
+        }}
+      />
+
+      <BottomStack.Screen name="Shop" component={Shop}
+        options={{
+          tabBarIcon: ({ color, focused }) => <ShopIcon bgColor={focused ? color : 'none'} color={color} />,
+        }}
+      />
+
+      <BottomStack.Screen name="Bag" component={Bag}
+        options={{
+          tabBarIcon: ({ color, focused }) => <BagIcon bgColor={focused ? color : 'none'} color={color} />,
+        }}
+      />
+
+      <BottomStack.Screen name="Profile" component={Profile}
+        options={{
+          tabBarIcon: ({ color, focused }) => <ProfileIcon bgColor={focused ? color : 'none'} color={color} />,
+        }}
+      />
+
+    </BottomStack.Navigator>
+  );
+}
+
+export default Tabs;
