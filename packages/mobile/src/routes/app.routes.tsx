aff23d058e60fcd0b166180c7aa37777da7c9fa7
danielpoliveira <dani.edm@outlook.com>
2020-09-18 23:13:09 -0300

    Added Product page to app routes

--- packages/mobile/src/routes/app.routes.tsx
+++ packages/mobile/src/routes/app.routes.tsx
@@ -1,78 +1,40 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import Home from '@pages/Home';
-import Shop from '@pages/Shop';
-import Bag from '@pages/Bag';
-import Profile from '@pages/Profile';
-
-import HomeIcon from '@components/BottomTab/Icons/HomeIcon';
-import ShopIcon from '@components/BottomTab/Icons/ShopIcon';
-import BagIcon from '@components/BottomTab/Icons/BagIcon';
-import ProfileIcon from '@components/BottomTab/Icons/ProfileIcon';
+import Tabs from '@routes/bottomNavigation.routes';
+import Product from '@pages/Product';
 
 import { useTheme } from '@contexts/theme';
 
-const AppStack = createBottomTabNavigator();
+const AppStack = createStackNavigator();
 
-const AppRoutes: React.FC = () => {
-
+const AppRoutes = (props) => {
   const { theme } = useTheme();
-
   return (
-    <AppStack.Navigator
-      tabBarOptions={{
-        showLabel: false,
-        activeTintColor: theme.primary,
-        inactiveTintColor: theme.textColorSecundary,
-
-        iconStyle: {
-          //marginTop: 2,
-        },
-
-        labelStyle: {
-          //fontSize: 12,
-          //margin: 0,
-          //padding: 0,
-          
-          fontFamily: 'Poppins-SemiBold',
-        },
-
-        style: {
-          height: 60,
-          backgroundColor: theme.bottomBarBackground,
-          borderTopWidth: 0,
-          borderTopLeftRadius: 12,
-          borderTopRightRadius: 12,
-        },
-      }} >
-
-      <AppStack.Screen name="Home" component={Home}
+    <AppStack.Navigator screenOptions={{
+      headerTitleStyle: {
+        fontFamily: 'Archivo-Regular'
+      }
+    }}>
+      <AppStack.Screen
+        name="Bottom Tabs"
+        component={Tabs}
         options={{
-          tabBarIcon: ({ color, focused }) => <HomeIcon bgColor={focused ? color : 'none'} color={color} />,
+          headerShown: false,
         }}
       />
-
-      <AppStack.Screen name="Shop" component={Shop}
+      <AppStack.Screen
+        name="Product"
+        component={Product}
         options={{
-          tabBarIcon: ({ color, focused }) => <ShopIcon bgColor={focused ? color : 'none'} color={color} />,
+          headerTintColor: theme.textColor, 
+
+          headerTitleAlign: 'center'
         }}
       />
-
-      <AppStack.Screen name="Bag" component={Bag}
-        options={{
-          tabBarIcon: ({ color, focused }) => <BagIcon bgColor={focused ? color : 'none'} color={color} />,
-        }}
-      />
-
-      <AppStack.Screen name="Profile" component={Profile}
-        options={{
-          tabBarIcon: ({ color, focused }) => <ProfileIcon bgColor={focused ? color : 'none'} color={color} />,
-        }}
-      />
-
     </AppStack.Navigator>
   );
-}
+};
+
 export default AppRoutes;
+
