1362812f4cc75dc8fa657a0404619174b895c056
danielpoliveira <dani.edm@outlook.com>
2020-09-18 23:10:22 -0300

    Scrollview added around components

--- packages/mobile/src/pages/Profile/index.tsx
+++ packages/mobile/src/pages/Profile/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-import Title from '@components/Title';
+import { View, StatusBar, ScrollView } from 'react-native';
 
+import Title from '@components/Title';
 import { AvatarView, LineSelection } from './styles';
 
-const Profile: React.FC = (props) => {
-
+const Profile = props => {
+  
   return (
     <View style={{
       flex: 1,
@@ -18,22 +18,29 @@ const Profile: React.FC = (props) => {
         <View style={{ marginBottom: 10 }}>
           <Title placeholder="My Profile" />
         </View>
+      </View>
 
-        <View style={{ marginBottom: 40 }}>
-          <AvatarView name="Daniel Oliveira" email="dani.edm@outlook.com" />
+      <ScrollView>
+        <View style={{
+          paddingHorizontal: 15,
+        }} >
+
+          <View style={{ marginBottom: 40 }}>
+            <AvatarView name="Daniel Oliveira" email="dani.edm@outlook.com" />
+          </View>
         </View>
 
-      </View>
+        <View>
+          <LineSelection title="My orders" description="Already have 12 orders" />
+          <LineSelection title="Shipping addresses" description="3 ddresses" />
+          <LineSelection title="Payment methods" description="Visa  **34" />
+          <LineSelection title="Promocodes" description="You have special promocodes" />
+          <LineSelection title="My reviews" description="Reviews for 4 items" />
 
-      <View>
-        <LineSelection title="My orders" description="Already have 12 orders" />
-        <LineSelection title="Shipping addresses" description="3 ddresses" />
-        <LineSelection title="Payment methods" description="Visa  **34" />
-        <LineSelection title="Promocodes" description="You have special promocodes" />
-        <LineSelection title="My reviews" description="Reviews for 4 items" />
+          <LineSelection title="Settings" description="Notifications, password" end />
+        </View>
 
-        <LineSelection title="Settings" description="Notifications, password" end />
-      </View>
+      </ScrollView>
 
     </View>
   );
