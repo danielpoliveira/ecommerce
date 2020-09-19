cdade86be8fcd16f821ba537a19327aca0d05999
danielpoliveira <dani.edm@outlook.com>
2020-09-18 23:11:56 -0300

    Added NavigationContainer to routes file

--- packages/mobile/src/routes/index.tsx
+++ packages/mobile/src/routes/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { useAuth  } from '@contexts/auth';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { useAuth } from '@contexts/auth';
+import { useTheme } from '@contexts/theme';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
 
-const Routes: React.FC = () => {
+const Routes = () => {
   const { signed } = useAuth();
+  const { theme } = useTheme();
 
-  return !signed? <AppRoutes /> : <AuthRoutes />
+  return (
+    <NavigationContainer theme={navigationStyles(theme)}>
+      { !signed ? <AppRoutes /> : <AuthRoutes />}
+    </NavigationContainer>
+  );
 }
 
+const navigationStyles = theme => ({
+  colors: {
+    background: theme.background,
+  }
+});
 
 export default Routes;
